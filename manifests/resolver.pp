862143f092b7bed4b9ad51fd7281ff23919f4b28
Keith Burdis <keith@burdis.org>
2012-10-23 15:38:28 +0100

    Use a pdns_resolver hash for hiera config rather than long variable names.

--- manifests/resolver.pp
+++ manifests/resolver.pp
@@ -3,6 +3,9 @@ class pdns::resolver(
   $dont_query     = undef,
   $forward_zones  = undef,
   $use_hiera      = false,
+  $forward_domain = undef,
+  $reverse_domain = undef,
+  $nameserver     = undef
 ) {
   # Only run on RedHat derived systems.
   case $::osfamily {
@@ -12,10 +15,14 @@ class pdns::resolver(
     }
   }
   if $use_hiera {
+    $pdns_resolver = hiera('pdns_resolver')
     class { 'pdns::resolver::config':
-      listen_address => hiera('ntp_servers', $listen_address),
-      dont_query     => hiera('ntp_servers', $dont_query),
-      forward_zones  => hiera('ntp_servers', $forward_zones),
+      listen_address => $pdns_resolver['listen_address'],
+      dont_query     => $pdns_resolver['dont_query'],
+      forward_zones  => $pdns_resolver['forward_zones'],
+      forward_domain => $pdns_resolver['forward_domain'],
+      reverse_domain => $pdns_resolver['reverse_domain'],
+      nameserver     => $pdns_resolver['nameserver'],
     } 
   }
   else {
@@ -23,6 +30,9 @@ class pdns::resolver(
       listen_address => $listen_address,
       dont_query     => $dont_query,
       forward_zones  => $forward_zones,
+      forward_domain => $forward_domain,
+      reverse_domain => $reverse_domain,
+      nameserver     => $nameserver,
     } 
   }
   require pdns::resolver::install
