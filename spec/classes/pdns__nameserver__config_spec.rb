ffe596f224616dd17c5c26c4669f684638a0dfe6
Keith Burdis <keith@burdis.org>
2012-10-24 17:00:23 +0100

    Updated tests based on latest template changes.

--- spec/classes/pdns__nameserver__config_spec.rb
+++ spec/classes/pdns__nameserver__config_spec.rb
@@ -24,8 +24,8 @@ describe 'pdns::nameserver::config', :type => 'class' do
       } )
       should contain_file('/var/pdns/dbsetup.sh').with( {
         'ensure'  => 'present',
-        'mode'    => '0500',
-        'content' => "#!/bin/bash\nservice postgresql initdb\nservice postgresql start\nchkconfig postgresql on\nsudo -H -u postgres createdb powerdns\nsudo -H -u postgres psql powerdns < /etc/pdns/schema.sql\nSQL_FILE=$(mktemp '/tmp/pdnsdb-setup.XXXXXXXXXX')\n>$SQL_FILE\ncat <<EOT >>$SQL_FILE\nCREATE USER pdns PASSWORD O1HDV1MiJGkP066j;\nGRANT SELECT ON supermasters TO pdns;\nGRANT ALL ON domains TO pdns;\nGRANT ALL ON domains_id_seq TO pdns;\nGRANT ALL ON records TO pdns;\nGRANT ALL ON records_id_seq TO pdns;\nEOT\nsudo -H -u postgres psql powerdns < $SQL_FILE\nrm -f $SQL_FILE\n",
+        'mode'    => '0555',
+        'content' => "#!/bin/bash\nexec >/var/pdns/dbsetup.log\nexec 2>&1\nset -x\nset -e\nif test ! -f /var/lib/pgsql/data/PG_VERSION; then\n  service postgresql initdb\nfi\nservice postgresql start\nchkconfig postgresql on\nsu -c 'createdb powerdns' postgres\nsu -c 'psql powerdns < /var/pdns/schema.sql' postgres\nSQL_FILE=$(mktemp '/tmp/pdnsdb-setup.XXXXXXXXXX')\n>$SQL_FILE\ncat <<EOT >>$SQL_FILE\nCREATE USER pdns;\nGRANT SELECT ON supermasters TO pdns;\nGRANT ALL ON domains TO pdns;\nGRANT ALL ON domains_id_seq TO pdns;\nGRANT ALL ON records TO pdns;\nGRANT ALL ON records_id_seq TO pdns;\nEOT\nchmod ugo+r $SQL_FILE\nsu -c \"psql powerdns < $SQL_FILE\" postgres\nrm -f $SQL_FILE\nset +e\nexit 0\n",
       } )
     }
   end
@@ -48,8 +48,8 @@ describe 'pdns::nameserver::config', :type => 'class' do
       } )
       should contain_file('/var/pdns/dbsetup.sh').with( {
         'ensure'  => 'present',
-        'mode'    => '0500',
-        'content' => "#!/bin/bash\nsqlite3 /var/pdns/powerdns.sqlite < /etc/pdns/schema.sql\nSQL_FILE=$(mktemp '/tmp/pdnsdb-setup.XXXXXXXXXX')\n>$SQL_FILE\nrm -f $SQL_FILE\n",
+        'mode'    => '0555',
+        'content' => "#!/bin/bash\nexec >/var/pdns/dbsetup.log\nexec 2>&1\nset -x\nset -e\nsqlite3 /var/pdns/powerdns.sqlite < /var/pdns/schema.sql\nSQL_FILE=$(mktemp '/tmp/pdnsdb-setup.XXXXXXXXXX')\n>$SQL_FILE\nsqlite3 /var/pdns/powerdns.sqlite < $SQL_FILE\nrm -f $SQL_FILE\nset +e\nexit 0\n",
       } )
     }
   end
@@ -73,8 +73,8 @@ describe 'pdns::nameserver::config', :type => 'class' do
       } )
       should contain_file('/var/pdns/dbsetup.sh').with( {
         'ensure'  => 'present',
-        'mode'    => '0500',
-        'content' => "#!/bin/bash\nservice postgresql initdb\nservice postgresql start\nchkconfig postgresql on\nsudo -H -u postgres createdb powerdns\nsudo -H -u postgres psql powerdns < /etc/pdns/schema.sql\nSQL_FILE=$(mktemp '/tmp/pdnsdb-setup.XXXXXXXXXX')\n>$SQL_FILE\ncat <<EOT >>$SQL_FILE\nCREATE USER pdns PASSWORD O1HDV1MiJGkP066j;\nGRANT SELECT ON supermasters TO pdns;\nGRANT ALL ON domains TO pdns;\nGRANT ALL ON domains_id_seq TO pdns;\nGRANT ALL ON records TO pdns;\nGRANT ALL ON records_id_seq TO pdns;\nEOT\ncat <<EOT >>$SQL_FILE\nINSERT INTO \"domains\" (name, type) VALUES('local','MASTER');\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'local','NS','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'local','SOA','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'ns1.local','A','10.0.0.2',3600,NULL);\nINSERT INTO \"domains\" (name, type) VALUES('10.in-addr.arpa','MASTER');\nINSERT INTO \"records\" (reverse_id, name, type, content, ttl, prio) VALUES(1,'10.in-addr.arpa','NS','10.in-addr.arpa',86400,NULL);\nINSERT INTO \"records\" (reverse_id, name, type, content, ttl, prio) VALUES(1,'10.in-addr.arpa','SOA','10.in-addr.arpa',86400,NULL);\nINSERT INTO \"records\" (reverse_id, name, type, content, ttl, prio) VALUES(1,'10.in-addr.arpa','A','10.0.0.2',3600,NULL);\nEOT\nsudo -H -u postgres psql powerdns < $SQL_FILE\nrm -f $SQL_FILE\n",
+        'mode'    => '0555',
+        'content' => "#!/bin/bash\nexec >/var/pdns/dbsetup.log\nexec 2>&1\nset -x\nset -e\nif test ! -f /var/lib/pgsql/data/PG_VERSION; then\n  service postgresql initdb\nfi\nservice postgresql start\nchkconfig postgresql on\nsu -c 'createdb powerdns' postgres\nsu -c 'psql powerdns < /var/pdns/schema.sql' postgres\nSQL_FILE=$(mktemp '/tmp/pdnsdb-setup.XXXXXXXXXX')\n>$SQL_FILE\ncat <<EOT >>$SQL_FILE\nCREATE USER pdns;\nGRANT SELECT ON supermasters TO pdns;\nGRANT ALL ON domains TO pdns;\nGRANT ALL ON domains_id_seq TO pdns;\nGRANT ALL ON records TO pdns;\nGRANT ALL ON records_id_seq TO pdns;\nEOT\nREVERSE_IP=$(perl -e 'my @f = split(/\\./, $ARGV[0]); print join(\".\", reverse @f)' 10.0.0.2)\ncat <<EOT >>$SQL_FILE\nINSERT INTO \"domains\" (name, type) VALUES('local','MASTER');\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'local','NS','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'local','SOA','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'ns1.local','A','10.0.0.2',3600,NULL);\nINSERT INTO \"domains\" (name, type) VALUES('10.in-addr.arpa','MASTER');\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(2,'10.in-addr.arpa','NS','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(2,'10.in-addr.arpa','SOA','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(2,'${REVERSE_IP}.in-addr.arpa','PTR','ns1.local',3600,NULL);\nEOT\nchmod ugo+r $SQL_FILE\nsu -c \"psql powerdns < $SQL_FILE\" postgres\nrm -f $SQL_FILE\nset +e\nexit 0\n",
       } )
     }
   end
@@ -98,8 +98,8 @@ describe 'pdns::nameserver::config', :type => 'class' do
       } )
       should contain_file('/var/pdns/dbsetup.sh').with( {
         'ensure'  => 'present',
-        'mode'    => '0500',
-        'content' => "#!/bin/bash\nsqlite3 /var/pdns/powerdns.sqlite < /etc/pdns/schema.sql\nSQL_FILE=$(mktemp '/tmp/pdnsdb-setup.XXXXXXXXXX')\n>$SQL_FILE\ncat <<EOT >>$SQL_FILE\nINSERT INTO \"domains\" (name, type) VALUES('local','MASTER');\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'local','NS','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'local','SOA','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'ns1.local','A','10.0.0.2',3600,NULL);\nINSERT INTO \"domains\" (name, type) VALUES('10.in-addr.arpa','MASTER');\nINSERT INTO \"records\" (reverse_id, name, type, content, ttl, prio) VALUES(1,'10.in-addr.arpa','NS','10.in-addr.arpa',86400,NULL);\nINSERT INTO \"records\" (reverse_id, name, type, content, ttl, prio) VALUES(1,'10.in-addr.arpa','SOA','10.in-addr.arpa',86400,NULL);\nINSERT INTO \"records\" (reverse_id, name, type, content, ttl, prio) VALUES(1,'10.in-addr.arpa','A','10.0.0.2',3600,NULL);\nEOT\nrm -f $SQL_FILE\n",
+        'mode'    => '0555',
+        'content' => "#!/bin/bash\nexec >/var/pdns/dbsetup.log\nexec 2>&1\nset -x\nset -e\nsqlite3 /var/pdns/powerdns.sqlite < /var/pdns/schema.sql\nSQL_FILE=$(mktemp '/tmp/pdnsdb-setup.XXXXXXXXXX')\n>$SQL_FILE\nREVERSE_IP=$(perl -e 'my @f = split(/\\./, $ARGV[0]); print join(\".\", reverse @f)' 10.0.0.2)\ncat <<EOT >>$SQL_FILE\nINSERT INTO \"domains\" (name, type) VALUES('local','MASTER');\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'local','NS','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'local','SOA','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(1,'ns1.local','A','10.0.0.2',3600,NULL);\nINSERT INTO \"domains\" (name, type) VALUES('10.in-addr.arpa','MASTER');\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(2,'10.in-addr.arpa','NS','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(2,'10.in-addr.arpa','SOA','ns1.local',86400,NULL);\nINSERT INTO \"records\" (domain_id, name, type, content, ttl, prio) VALUES(2,'${REVERSE_IP}.in-addr.arpa','PTR','ns1.local',3600,NULL);\nEOT\nsqlite3 /var/pdns/powerdns.sqlite < $SQL_FILE\nrm -f $SQL_FILE\nset +e\nexit 0\n",
       } )
     }
   end
